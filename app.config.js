7a7c040b35502d5af6614951a97301221a56422d
garettie <coldsnp@gmail.com>
2026-08-07 10:18:55 +0800

    change project ID

--- app.config.js
+++ app.config.js
@@ -3,16 +3,16 @@ const { expo } = require("./app.json");
 const isDevelopment = process.env.APP_VARIANT === "development";
 
 module.exports = {
-	...expo,
-	name: isDevelopment ? "Eatlog Dev" : expo.name,
-	updates: {
-		url: "https://u.expo.dev/bffe0ee2-d580-4ad2-8fa1-ae76f16279c3",
-	},
-	runtimeVersion: {
-		policy: "appVersion",
-	},
-	android: {
-		...expo.android,
-		package: isDevelopment ? "com.sgaret.eatlog.dev" : expo.android.package,
-	},
+  ...expo,
+  name: isDevelopment ? "Eatlog Dev" : expo.name,
+  updates: {
+    url: "https://u.expo.dev/700befb6-016e-4d35-a35c-bd375da07e4d",
+  },
+  runtimeVersion: {
+    policy: "appVersion",
+  },
+  android: {
+    ...expo.android,
+    package: isDevelopment ? "com.sgaret.eatlog.dev" : expo.android.package,
+  },
 };
